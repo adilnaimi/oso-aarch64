656f7f793cb84b167a2d7da1c652baf6b41f2265
Adil Naimi <adilnaimi@gmail.com>
2024-01-11 14:28:35 +0100

    oso v0.27.2

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOCKER_BUILDKIT=1 docker build \
+    --output amazonlinux \
+    --build-arg="OSO_VERSION=0.27.2" \
+    --file Dockerfile.amazonlinux \
+    --target runtime \
+    .
+
+DOCKER_BUILDKIT=1 docker build \
+    --output debian \
+    --build-arg="OSO_VERSION=0.27.2" \
+    --file Dockerfile.debian \
+    --target runtime \
+    .
